63c9cb947c2baa8ab809ad0e28b4060ad16ba4be
reni <reneta.marinova97@gmail.com>
2023-11-13 10:55:40 +0200

    Implementing Status Enum

--- src/com/company/oop/taskmanagementsystem/models/enums/Status.java
+++ src/com/company/oop/taskmanagementsystem/models/enums/Status.java
@@ -1,4 +1,33 @@
 package com.company.oop.taskmanagementsystem.models.enums;
 
 public enum Status {
+    NEW,
+    UNSCHEDULED,
+    SCHEDULED,
+    ACTIVE,
+    NOT_DONE,
+    IN_PROGRESS,
+    DONE;
+
+    @Override
+    public String toString() {
+        switch (this) {
+            case NEW:
+                return "New";
+            case UNSCHEDULED:
+                return "Unscheduled";
+            case SCHEDULED:
+                return "Scheduled";
+            case ACTIVE:
+                return "Active";
+            case NOT_DONE:
+                return "Not done";
+            case IN_PROGRESS:
+                return "In progress";
+            case DONE:
+                return "Done";
+            default:
+                throw new UnsupportedOperationException("There is no such status.");
+        }
+    }
 }
