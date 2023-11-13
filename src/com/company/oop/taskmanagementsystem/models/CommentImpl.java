2672ae0ba37d7657d1e63d302cdaad99df7e9864
TeodorK0907 <ted_kehayov@abv.bg>
2023-11-13 21:36:23 +0200

    Updated CommentImpl

--- src/com/company/oop/taskmanagementsystem/models/CommentImpl.java
+++ src/com/company/oop/taskmanagementsystem/models/CommentImpl.java
@@ -1,22 +1,41 @@
 package com.company.oop.taskmanagementsystem.models;
 
 import com.company.oop.taskmanagementsystem.models.contracts.Comment;
+import com.company.oop.taskmanagementsystem.utils.ValidationHelpers;
 
 public class CommentImpl implements Comment {
-
+    private static final int CONTENT_MIN_LENGTH = 1;
+    private static final int CONTENT_MAX_LENGTH = 500;
+    private static final String CONTENT_LENGTH_ERROR_MESSAGE = "Comment needs to be between 1 and 500 symbols";
+    public static final String NEW_LINE_DASH = "----------";
     private String author;
     private String content;
     // todo unsure of parameters order
     public CommentImpl(String author, String content){
-
+        this.author = author;
+        setContent(content);
     }
     @Override
     public String getAuthor() {
-        return null;
+        return this.author;
     }
 
     @Override
     public String getContent() {
-        return null;
+        return this.content;
+    }
+
+    private void setContent(String content){
+        ValidationHelpers.validateIntRange(content.length(), CONTENT_MIN_LENGTH,
+                CONTENT_MAX_LENGTH, CONTENT_LENGTH_ERROR_MESSAGE);
+    }
+
+    @Override
+    public String print() {
+        StringBuilder output = new StringBuilder(NEW_LINE_DASH).append(System.lineSeparator());
+        output.append(String.format("%s", getContent())).append(System.lineSeparator())
+                .append(String.format("User: %s", getAuthor())).append(System.lineSeparator())
+                .append(NEW_LINE_DASH);
+        return output.toString();
     }
 }
