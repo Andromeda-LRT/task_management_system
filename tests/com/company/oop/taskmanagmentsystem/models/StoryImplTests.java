65f7ed84205a1ce88928da66906ce1a3e44c6276
TeodorK0907 <ted_kehayov@abv.bg>
2023-11-14 18:10:56 +0200

    Added Tests for Severity, Status, Priority and Size methods

--- tests/com/company/oop/taskmanagmentsystem/models/StoryImplTests.java
+++ tests/com/company/oop/taskmanagmentsystem/models/StoryImplTests.java
@@ -2,11 +2,14 @@ package com.company.oop.taskmanagmentsystem.models;
 
 import com.company.oop.taskmanagementsystem.models.BugImpl;
 import com.company.oop.taskmanagementsystem.models.CommentImpl;
+import com.company.oop.taskmanagementsystem.constants.Constants;
 import com.company.oop.taskmanagementsystem.models.StoryImpl;
 import com.company.oop.taskmanagementsystem.models.TaskImpl;
 import com.company.oop.taskmanagementsystem.models.contracts.Comment;
 import com.company.oop.taskmanagementsystem.models.contracts.Story;
 import com.company.oop.taskmanagementsystem.models.contracts.Task;
+import com.company.oop.taskmanagementsystem.models.enums.Priority;
+import com.company.oop.taskmanagementsystem.models.enums.Size;
 import com.company.oop.taskmanagementsystem.models.enums.Status;
 import com.company.oop.taskmanagmentsystem.constants.TestsConstants;
 import org.junit.jupiter.api.Assertions;
@@ -68,12 +71,86 @@ public class StoryImplTests {
                         TestsConstants.TEST_PRIORITY,
                         TestsConstants.TEST_SIZE));
     }
-    //todo tests with advance and revert of status, priority and size
-    // todo tests with comments and logger
+   @Test
+   public void getComments_Should_ReturnACopyOfCommentsList(){
+        Assertions.assertNotNull(story.getComments());
+   }
     @Test
-    public void advanceStatus_ShouldAdvance_When_StatusIsValid(){
-       // Assertions.assertEquals(Status.IN_PROGRESS, story.);
+    public void advanceStatus_Should_AdvanceStatus_WhenValid(){
+        story.advanceStatus();
+        Assertions.assertEquals(Status.IN_PROGRESS, story.getStatus());
     }
+
+    @Test
+    public void revertStatus_Should_RevertStatus_WhenValid(){
+        story.advanceStatus();
+        story.revertStatus();
+        Assertions.assertEquals(Status.NOT_DONE, story.getStatus());
+    }
+    @Test
+    public void advanceStatus_Should_GiveAnErrorMessage_When_StatusCannotAdvanceFurther(){
+        story.advanceStatus();
+        story.advanceStatus();
+        story.advanceStatus();
+        Assertions.assertEquals(String.format(Constants.STATUS_IS_ALREADY_SET_TO_DONE, Constants.STORY),
+                story.getHistoryOfChanges().get(3).getDescription());
+    }
+    @Test
+    public void revertStatus_Should_GiveAnErrorMessage_When_StatusCannotRevertFurther(){
+        story.revertStatus();
+        Assertions.assertEquals(String.format(Constants.STATUS_IS_ALREADY_SET_TO_NOT_DONE, Constants.STORY),
+                story.getHistoryOfChanges().get(1).getDescription());
+    }
+    @Test
+    public void increasePriority_Should_IncreasePriority_When_PriorityIsValid(){
+        story.increasePriority();
+        Assertions.assertEquals(Priority.MEDIUM, story.getPriority());
+    }
+    @Test
+    public void lowerPriority_Should_LowerPriority_When_PriorityIsValid(){
+        story.increasePriority();
+        story.lowerPriority();
+        Assertions.assertEquals(Priority.LOW, story.getPriority());
+    }
+    @Test
+    public void increasePriority_Should_GiveAnErrorMessage_When_PriorityCannotIncreaseFurther(){
+        story.increasePriority();
+        story.increasePriority();
+        story.increasePriority();
+        Assertions.assertEquals(String.format(Constants.PRIORITY_IS_ALREADY_SET_TO_HIGH, Constants.STORY),
+                story.getHistoryOfChanges().get(3).getDescription());
+    }
+    @Test
+    public void lowerPriority_Should_GiveAnErrorMessage_When_PriorityCannotLowerFurther(){
+        story.lowerPriority();
+        Assertions.assertEquals(String.format(Constants.PRIORITY_IS_ALREADY_SET_TO_LOW, Constants.STORY),
+                story.getHistoryOfChanges().get(1).getDescription());
+    }
+    @Test
+    public void increaseSize_Should_IncreaseSize_When_SizeIsValid(){
+        story.increaseSize();
+        Assertions.assertEquals(Size.LARGE, story.getSize());
+    }
+    @Test
+    public void decreaseSize_Should_DecreaseSize_When_SizeIsValid(){
+        story.decreaseSize();
+        Assertions.assertEquals(Size.SMALL, story.getSize());
+    }
+    @Test
+    public void increaseSize_Should_GiveAnErrorMessage_When_SizeCannotIncreaseFurther(){
+        story.increaseSize();
+        story.increaseSize();
+        Assertions.assertEquals(Constants.SIZE_ALREADY_SET_TO_LARGE,
+                story.getHistoryOfChanges().get(2).getDescription());
+    }
+    @Test void decreaseSize_Should_GiveAnErrorMessage_When_SizeCannotDecreaseFurther(){
+        story.decreaseSize();
+        story.decreaseSize();
+               Assertions.assertEquals(Constants.SIZE_ALREADY_SET_TO_SMALL,
+                story.getHistoryOfChanges().get(2).getDescription());
+    }
+
+    // todo tests with logger
 
     @Test
     public void addComment_Should_AddCommentToList() {
