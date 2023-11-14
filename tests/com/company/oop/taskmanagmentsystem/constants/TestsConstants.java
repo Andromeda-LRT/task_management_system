55a2a7fa9f4b3089293d55f11d2d5b6183692d6c
TeodorK0907 <ted_kehayov@abv.bg>
2023-11-14 11:36:29 +0200

    Created new Test Constants and added several tests for StoryImpl

--- tests/com/company/oop/taskmanagmentsystem/constants/TestsConstants.java
+++ tests/com/company/oop/taskmanagmentsystem/constants/TestsConstants.java
@@ -1,5 +1,8 @@
 package com.company.oop.taskmanagmentsystem.constants;
 
+import com.company.oop.taskmanagementsystem.models.enums.Priority;
+import com.company.oop.taskmanagementsystem.models.enums.Size;
+
 public class TestsConstants {
     public static final int VALID_ID = 1;
     public static final String VALID_TITLE = "Example of a valid title";
@@ -7,4 +10,6 @@ public class TestsConstants {
     public static final int VALID_RATING = 10;
     public static final String INVALID_TITLE = "Invalid";
     public static final String INVALID_DESCRIPTION = "Invalid";
+    public static final Priority TEST_PRIORITY = Priority.LOW;
+    public static final Size TEST_SIZE = Size.MEDIUM;
 }
