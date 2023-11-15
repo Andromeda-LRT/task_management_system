c8183b3f5bff31ae515e20445872bc7de0d5ac18
TeodorK0907 <ted_kehayov@abv.bg>
2023-11-15 14:05:23 +0200

    Added methods for task assignment and unassignment to Board interface and created implementation for both in BoardImpl class.

--- src/com/company/oop/taskmanagementsystem/models/BoardImpl.java
+++ src/com/company/oop/taskmanagementsystem/models/BoardImpl.java
@@ -15,9 +15,14 @@ public class BoardImpl implements Board {
             "Task %s has been added to board with name %s";
     private static final String TASK_REMOVED =
             "Task %s has been removed from board with name %s";
+    private static final String TASK_ASSIGNED =
+            "Task %s with description:%n has been assigned to %s";
+    private static final String TASK_UNASSIGNED =
+            "Task %s with description:%n has been unassigned from %s";
     private String name;
     private List<LoggerImpl> activityHistory;
     private List<TaskImpl> taskList;
+    // teamsImpl teamsList List<TeamsImpl> teamsList;
 
     public BoardImpl(String name){
         setName(name);
@@ -45,19 +50,25 @@ public class BoardImpl implements Board {
                 BOARD_NAME_MAX_LENGTH, INVALID_BOARD_NAME_MESSAGE);
         this.name = name;
     }
-
+    @Override
     public void addTask(TaskImpl task){
         taskList.add(task);
         logChange(String.format(TASK_ADDED, task.getTitle(), getName()));
     }
+    @Override
     public void removeTask(TaskImpl task){
-        taskList.remove(task);
+        for (TaskImpl taskLocal : taskList) {
+            if (taskLocal == task){
+                taskList.remove(task);
+            }
+        }
+        //taskList.remove(task);
         logChange(String.format(TASK_REMOVED, task.getTitle(), getName()));
     }
     private void logChange(String change){
         activityHistory.add(new LoggerImpl(change));
     }
-    
+    @Override
     public String showBoardActivity(){
         StringBuilder output = new StringBuilder(toString());
         for (LoggerImpl logger : activityHistory) {
@@ -65,6 +76,56 @@ public class BoardImpl implements Board {
             output.append(System.lineSeparator());
         }
         return output.toString().trim();
+    }
+
+    @Override
+    public void assignTask(TaskImpl task, MemberImpl memberToAssignTaskTo) {
+        /*
+        if (teamsList.isEmpty){
+        throw new IllegalArgumentException("Please create a team prior to assigning a task
+        to a member");
+        } else {
+        for (TeamsImpl teamsLocal : teamsList){
+        if( teamsLocal.getMembers.contains(memberToAssignTaskTo){
+            memberToAssignTaskTo.addTask(task);
+            logChange(String.format(TASK_ASSIGNED, task.getTitle(), task.getDescription,
+             memberToAssignTaskTo.getName()))
+            break;
+        } else {
+        throw new IllegalArgumentException("The member you want to assign a task to
+        does not exist/does not belong to a team");
+        }
+       }
+      }
+         */
+        // to have a for each loop for teamImpl list and to find the concrete member
+        // then to have it assigned
+        // add memberimpl as paramter to use teams list to look for the member
+        // in question.
+        // could throw an exception if teams list is empty
+        // throw exception if member does not exist in teams's list
+    }
+
+    @Override
+    public void unassignTask(TaskImpl task, MemberImpl memberToUnassignTask) {
+         /*
+        if (teamsList.isEmpty){
+        throw new IllegalArgumentException("Please create a team prior to assigning a task
+        to a member");
+        } else {
+        for (TeamsImpl teamsLocal : teamsList){
+        if( teamsLocal.getMembers.contains(memberToAssignTaskTo){
+            memberToAssignTaskTo.removeTask(task);
+            logChange(String.format(TASK_UNASSIGNED, task.getTitle(), task.getDescription,
+             memberToAssignTaskTo.getName()))
+            break;
+        } else {
+        throw new IllegalArgumentException("The member you want to assign a task to
+        does not exist/does not belong to a team");
+        }
+       }
+      }
+         */
     }
 
     @Override
--- src/com/company/oop/taskmanagementsystem/models/contracts/Board.java
+++ src/com/company/oop/taskmanagementsystem/models/contracts/Board.java
@@ -1,13 +1,20 @@
 package com.company.oop.taskmanagementsystem.models.contracts;
 
 import com.company.oop.taskmanagementsystem.models.LoggerImpl;
+import com.company.oop.taskmanagementsystem.models.MemberImpl;
 import com.company.oop.taskmanagementsystem.models.TaskImpl;
 
 import java.util.List;
 
 public interface Board {
     String getName();
-
     List<TaskImpl> getTaskList();
     List<LoggerImpl> getActivityHistory();
+    String showBoardActivity();
+   // List<TeamImpl> getTeams();
+    void assignTask(TaskImpl task, MemberImpl memberToAssignTaskTo);
+    void unassignTask(TaskImpl task, MemberImpl memberToUnassignTask);
+    void addTask(TaskImpl task);
+    void removeTask(TaskImpl task);
+
 }
