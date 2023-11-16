7c6ee5dfa025772bf378dfd2d4270086a6174cdd
LyubimaMalkocheva <lyubima.malkocheva@gmail.com>
2023-11-16 15:54:11 +0200

    Commands for team class

--- src/com/company/oop/taskmanagementsystem/commands/AddMemberToTeam.java
+++ src/com/company/oop/taskmanagementsystem/commands/AddMemberToTeam.java
@@ -8,7 +8,10 @@ import com.company.oop.taskmanagementsystem.utils.ValidationHelpers;
 
 import java.util.List;
 
-public class AddMemberToTeam extends CommandImpl{
+public class AddMemberToTeam extends CommandImpl {
+    private static final String MEMBER_ADDED_TO_TEAM = "Member %s was added to team %s!";
+
+    private static final int EXPECTED_NUMBER_OF_ARGUMENTS = 2;
 
     public AddMemberToTeam(TaskManagementSystemRepository taskManagementSystemRepository) {
         super(taskManagementSystemRepository);
@@ -16,6 +19,19 @@ public class AddMemberToTeam extends CommandImpl{
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String teamName = parameters.get(0);
+        String memberName = parameters.get(1);
+
+        return addMemberToTeam(teamName, memberName);
+    }
+
+    private String addMemberToTeam(String teamName, String memberName) {
+        Team team = getTaskManagementSystemRepository().findTeamByName(teamName);
+        Member member = getTaskManagementSystemRepository().findMemberByName(memberName);
+
+        team.addMember(member);
+
+        return String.format(MEMBER_ADDED_TO_TEAM, memberName, teamName);
     }
 }
--- src/com/company/oop/taskmanagementsystem/commands/CreateNewTeam.java
+++ src/com/company/oop/taskmanagementsystem/commands/CreateNewTeam.java
@@ -15,4 +15,14 @@ public class CreateNewTeam extends CommandImpl {
     public String execute(List<String> parameters) {
         return null;
     }
+
+    private String createTeam(String teamName) {
+        if (getTaskManagementSystemRepository().teamExist(teamName)) {
+            throw new IllegalArgumentException(String.format(TEAM_ALREADY_EXISTS, teamName));
+        }
+
+        getTaskManagementSystemRepository().createTeam(teamName);
+
+        return String.format(TEAM_CREATED, teamName);
+    }
 }
