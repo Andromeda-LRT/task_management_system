7cb53ab87b3cc46743937f724ee361cbf5449558
reni <reneta.marinova97@gmail.com>
2023-11-16 12:06:51 +0200

    Implementing create member command

--- src/com/company/oop/taskmanagementsystem/commands/CreateNewMember.java
+++ src/com/company/oop/taskmanagementsystem/commands/CreateNewMember.java
@@ -2,10 +2,15 @@ package com.company.oop.taskmanagementsystem.commands;
 
 import com.company.oop.taskmanagementsystem.commands.contracts.Command;
 import com.company.oop.taskmanagementsystem.core.contracts.TaskManagementSystemRepository;
+import com.company.oop.taskmanagementsystem.models.MemberImpl;
+import com.company.oop.taskmanagementsystem.models.contracts.Member;
+import com.company.oop.taskmanagementsystem.utils.ValidationHelpers;
 
 import java.util.List;
 
 public class CreateNewMember implements Command {
+    private static final int EXPECTED_NUMBER_OF_ARGUMENTS = 1;
+    private final static String MEMBER_REGISTERED = "Member %s registered successfully!";
     private final TaskManagementSystemRepository taskManagementSystemRepository;
 
     public CreateNewMember(TaskManagementSystemRepository taskManagementSystemRepository) {
@@ -14,6 +19,9 @@ public class CreateNewMember implements Command {
 
     @Override
     public String execute(List<String> parameters) {
-        return null;
+        ValidationHelpers.validateArgumentsCount(parameters, EXPECTED_NUMBER_OF_ARGUMENTS);
+        String name = parameters.get(0);
+        Member member = new MemberImpl(name);
+        return String.format(MEMBER_REGISTERED, member.getName());
     }
 }
