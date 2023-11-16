7b8090dd5b24a7847e9781ee95f3798020c3a952
LyubimaMalkocheva <lyubima.malkocheva@gmail.com>
2023-11-16 15:53:38 +0200

    Upload commands for Team class

--- src/com/company/oop/taskmanagementsystem/commands/AddMemberToTeam.java
+++ src/com/company/oop/taskmanagementsystem/commands/AddMemberToTeam.java
@@ -2,6 +2,9 @@ package com.company.oop.taskmanagementsystem.commands;
 
 import com.company.oop.taskmanagementsystem.commands.contracts.Command;
 import com.company.oop.taskmanagementsystem.core.contracts.TaskManagementSystemRepository;
+import com.company.oop.taskmanagementsystem.models.contracts.Member;
+import com.company.oop.taskmanagementsystem.models.contracts.Team;
+import com.company.oop.taskmanagementsystem.utils.ValidationHelpers;
 
 import java.util.List;
 
