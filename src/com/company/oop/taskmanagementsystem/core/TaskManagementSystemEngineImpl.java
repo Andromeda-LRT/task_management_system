1b53dc97a36bc31bd891ca49559661037208c7c2
TeodorK0907 <ted_kehayov@abv.bg>
2023-11-20 14:01:30 +0200

    Added a new method to EngineImpl's Extract CommentOrDescriptionParameters method to place back the extracted description or comment in the correct place to match the relevant command's requested parameter order.

--- src/com/company/oop/taskmanagementsystem/core/TaskManagementSystemEngineImpl.java
+++ src/com/company/oop/taskmanagementsystem/core/TaskManagementSystemEngineImpl.java
@@ -15,9 +15,7 @@ public class TaskManagementSystemEngineImpl implements TaskManagementSystemEngin
     private static final String SHUT_DOWN_COMMAND = "Exit";
     private static final String MAIN_SPLIT_SYMBOL = " ";
     private static final String OPEN_COMMENT_SYMBOL = "{{";
-    private static final String CLOSE_COMMENT_SYMBOL = "{{";
-    //todo to discuss if the below separator is necessary and if a new would be better.
-    private static final String REPORT_SEPARATOR = "####################";
+    private static final String CLOSE_COMMENT_SYMBOL = "}}";
 
     private final TaskManagementSystemRepository taskManagementSystemRepository;
     private final CommandFactory commandFactory;
@@ -88,38 +86,65 @@ public class TaskManagementSystemEngineImpl implements TaskManagementSystemEngin
      */
     private List<String> extractOperationParameters(String input){
         if (input.contains(OPEN_COMMENT_SYMBOL)){
-            return extractCommentParameters(input);
+            return extractCommentOrDescriptionParameters(input);
         }
         String[] commandParts = input.split(" ");
-        List<String> parameters = Arrays.asList(commandParts);
-        parameters.remove(0);
-        // converting the array to a List and removing first index since it is extract in the above method
-        // and will be used for command. To try it out as an alternative instead of using a for loop.
-        // if it does not work to switch back to a for loop.
-//        for (int i = 1; i < commandParts.length; i++) {
-//            parameters.add(commandParts[i]);
-//        }
+        List<String> parameters = new ArrayList<>();
+        for (int i = 1; i < commandParts.length; i++) {
+            parameters.add(commandParts[i]);
+        }
         return parameters;
     }
-    public List<String> extractCommentParameters(String fullOperation){
+
+    public List<String> extractCommentOrDescriptionParameters(String fullOperation){
         int indexOfFirstSeparator = fullOperation.indexOf(MAIN_SPLIT_SYMBOL);
         int indexOfOpenComment = fullOperation.indexOf(OPEN_COMMENT_SYMBOL);
         int indexOfCloseComment = fullOperation.indexOf(CLOSE_COMMENT_SYMBOL);
         List<String> parameters = new ArrayList<>();
         if (indexOfOpenComment >= 0){
-            parameters.add(fullOperation.substring
+            parameters.add(fullOperation
+                    .substring
                     (indexOfOpenComment + OPEN_COMMENT_SYMBOL.length(), indexOfCloseComment));
             fullOperation = fullOperation.replaceAll("\\{\\{.+(?=}})}}", "");
         }
         List<String> result = new ArrayList<>(
                 Arrays.asList(fullOperation.substring(indexOfFirstSeparator + 1).split(MAIN_SPLIT_SYMBOL)));
-        result.removeAll(Arrays.asList(" ","", null));
-        parameters.addAll(result);
-        return parameters;
+        if (result.contains("")){
+            return AddCommentOrDescriptionToParameters(result, parameters);
+        }
+        return result;
+    }
+    /**
+     * The method takes a List where it has a single element that is "" (empty) and another List that has a single
+     * element, which is either a comment or a description for a given task. It iterates through each element
+     * in the source List and once it reaches the index that is "" it adds the element from the target List.
+     * Even though the element is "" it still counts as such, therefore the newly added element is added in its place,
+     * but the "" element is moved to the following index along with the elements after it.
+     * Before the loop ends all elements that are " ", "" or null are removed from the source List and the loop breaks.
+     * E.g. if the source List is " (title) Test (description) "" (rating) 2 "
+     * target List is "this is a test description, and it should serve as such"
+     * the method will return a list of ["Test, this is a test description, and it should serve as such, 2"].
+     *
+     * @param source The list containing all the parameters where there used to be either a comment or a description
+     * for a task. Currently, that index is "" (empty).
+     * @param target The list where the extracted comment or description is separated only by relevant
+     * punctuation and taking a slot of a single element in a List.
+     * @return A list of parameters that now has either a description or comment as a single element inside,
+     * instead of several elements.
+     */
+    public List<String> AddCommentOrDescriptionToParameters(List<String> source, List<String> target){
+
+        for (String parameter : source) {
+            if (parameter.isEmpty()) {
+                source.add(source.indexOf(parameter), target.get(0));
+                source.removeAll(Arrays.asList(" ","", null));
+                break;
+            }
+        }
+        return source;
     }
 
     private void print(String input){
         System.out.println(input);
-        System.out.println(REPORT_SEPARATOR);
     }
 }
